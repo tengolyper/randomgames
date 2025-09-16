4c66f1dc8574f09ea5625e64c06ef9c71a378230
tengolyper <tengolyper@outlook.com>
2025-09-16 12:13:41 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 // You can replace these with your own embed codes (iframe, HTML, etc)
 const embeds = [
-  `<iframe src="https://www.example.com" width="100%" height="100%" frameborder="0"></iframe>`,
-  `<iframe src="https://www.wikipedia.org" width="100%" height="100%" frameborder="0"></iframe>`,
-  `<iframe src="https://www.openstreetmap.org" width="100%" height="100%" frameborder="0"></iframe>`,
+  
+<iframe src="https://www.jopi.com/embed.php?game=basket-random" frameborder="0" scrolling="no" width="800" height="600" ></iframe>
+" width="100%" height="100%" frameborder="0"></iframe>`,
+  
+<iframe src="https://www.jopi.com/embed.php?game=soccer-random" frameborder="0" scrolling="no" width="800" height="600" ></iframe>`
+
+  
+<iframe src="https://www.jopi.com/embed.php?game=boxing-random" frameborder="0" scrolling="no" width="800" height="600" ></iframe>
+" width="100%" height="100%" frameborder="0"></iframe>`,
+
+
+<iframe src="https://www.jopi.com/embed.php?game=volley-random" frameborder="0" scrolling="no" width="800" height="600" ></iframe>
+
   `<div style="padding:40px;">Replace this with your custom embed HTML!</div>`
 ];
 
