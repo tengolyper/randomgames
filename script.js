06a782757e89a2b2058aad3c98f0c4560caaede6
tengolyper <tengolyper@outlook.com>
2025-09-17 07:15:21 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const embeds = [
-  `<iframe src="https://www.jopi.com/embed.php?game=basket-random" frameborder="0" scrolling="no" width="800" height="600"></iframe>`,
-  `<iframe src="https://www.jopi.com/embed.php?game=soccer-random" frameborder="0" scrolling="no" width="800" height="600"></iframe>`,
-  `<iframe src="https://www.jopi.com/embed.php?game=boxing-random" frameborder="0" scrolling="no" width="800" height="600"></iframe>`,
-  `<iframe src="https://www.jopi.com/embed.php?game=volley-random" frameborder="0" scrolling="no" width="800" height="600"></iframe>`,
+  `<iframe src="https://www.jopi.com/embed.php?game=basket-random" frameborder="0" scrolling="no" width="100%" height="100%"></iframe>`,
+  `<iframe src="https://www.jopi.com/embed.php?game=soccer-random" frameborder="0" scrolling="no" width="100%" height="100%"></iframe>`,
+  `<iframe src="https://www.jopi.com/embed.php?game=boxing-random" frameborder="0" scrolling="no" width="100%" height="100%"></iframe>`,
+  `<iframe src="https://www.jopi.com/embed.php?game=volley-random" frameborder="0" scrolling="no" width="100%" height="100%"></iframe>`,
   `<div style="padding:40px;">Replace this with your custom embed HTML!</div>`
 ];
 
