de6fa0e1aa135005f069a464f7caad0bcdf683b5
tengolyper <tengolyper@outlook.com>
2025-09-17 07:23:25 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const websites = [
-  `<iframe src="https://www.nba.com" frameborder="0" scrolling="yes" width="100%" height="100%"></iframe>`,
-  `<iframe src="https://www.fifa.com" frameborder="0" scrolling="yes" width="100%" height="100%"></iframe>`,
-  `<iframe src="https://www.boxingscene.com" frameborder="0" scrolling="yes" width="100%" height="100%"></iframe>`,
-  `<iframe src="https://www.volleycountry.com" frameborder="0" scrolling="yes" width="100%" height="100%"></iframe>`,
-  `<iframe src="https://www.wikipedia.org" frameborder="0" scrolling="yes" width="100%" height="100%"></iframe>`
+  `<iframe src="https://html5.gamedistribution.com/bf1268dccb5d43e7970bb3edaa54afc8/?utm_source=jopi.com&utm_medium=basket-random&utm_campaign=block-and-redirect" frameborder="0" scrolling="yes" width="100%" height="100%"></iframe>`,
+  `<iframe src="https://html5.gamedistribution.com/308d826f20034d7b972f25258c8d0a44/?utm_source=jopi.com&utm_medium=soccer-random&utm_campaign=block-and-redirect" frameborder="0" scrolling="yes" width="100%" height="100%"></iframe>`,
+  `<iframe src="https://html5.gamedistribution.com/e8c02771085e4c8b9de3deda5e087e0e/?utm_source=jopi.com&utm_medium=boxing-random&utm_campaign=block-and-redirect" frameborder="0" scrolling="yes" width="100%" height="100%"></iframe>`,
+  `<iframe src="https://html5.gamedistribution.com/b6f03bdc505e4d78a17379a5a83e206b/?utm_source=jopi.com&utm_medium=volley-random&utm_campaign=block-and-redirect" frameborder="0" scrolling="yes" width="100%" height="100%"></iframe>`,
+  `<iframe src="tengolyper.github.io/logineer" frameborder="0" scrolling="yes" width="100%" height="100%"></iframe>`
 ];
 
 function showEmbed(index) {
